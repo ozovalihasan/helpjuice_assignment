97503b54c02f69c153e88c80d7aa974e01efae90
ozovalihasan <ozovalihasan@gmail.com>
2022-12-02 21:27:22 +0300

    Update SearchesController to search articles

--- app/controllers/articles/searches_controller.rb
+++ app/controllers/articles/searches_controller.rb
@@ -1,11 +1,16 @@
 class Articles::SearchesController < ApplicationController
   def index
-    @articles = Article.all
+    if search_params[:keywords].present?
+      @search = Search.create(search_params)
+      @articles = Article.search(search_params[:keywords])
+    else
+      @articles = Article.limit(5).order("RANDOM()")
+    end
   end
 
   private
   
   def search_params
-    params.require(:search).permit(:keywords)
+    params.permit(:keywords)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+
+  scope :search, -> (keywords) { where("body LIKE ?", "%#{keywords}%") }
 end
