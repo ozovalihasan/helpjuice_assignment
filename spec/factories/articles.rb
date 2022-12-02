3b3567d195d2da9b023037a4d057d9ea481f5cc3
ozovalihasan <ozovalihasan@gmail.com>
2022-12-02 20:33:24 +0300

    Update the factory of Article

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :article do
-    title { Faker::Lorem.sentence(word_count: 3) + Search.all.sample.keywords + Faker::Lorem.sentence(word_count: 3) }
-    body { Faker::Quote.matz }
+    title { Faker::Lorem.sentence(word_count: 3, random_words_to_add: 4) }
+    body { Faker::Quote.matz + Search.all.sample.keywords + Faker::Quote.famous_last_words }
   end
 end
 
 FactoryBot.define do
   factory :mock_article, class: "Article" do
-    sequence(:title) { |n| "title_#{n} keywords_#{n}" }
-    sequence(:body) { |n| "body_#{n}" }
+    sequence(:title) { |n| "title_#{n}" }
+    sequence(:body) { |n| "body_#{n} keywords_#{n}" }
   end
 end
