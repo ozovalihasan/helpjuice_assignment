f5520d88c6a3d1f1a860bff627f1627cf2f1f21e
ozovalihasan <ozovalihasan@gmail.com>
2022-12-03 20:57:23 +0300

    Add a request test for Articles::KeywordsController

--- app/controllers/articles/keywords_controller.rb
+++ app/controllers/articles/keywords_controller.rb
@@ -1,5 +1,6 @@
 class Articles::KeywordsController < ApplicationController
   def index
+    redirect_to root_path unless turbo_frame_request?
     @searches = Search.order(created_at: :desc)
   end
 end
