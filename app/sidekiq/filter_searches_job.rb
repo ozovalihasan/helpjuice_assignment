24adb990889746e87fa75056978b6fdb848286d8
ozovalihasan <ozovalihasan@gmail.com>
2022-12-03 16:36:53 +0300

    Update FilterSearchesJob for edge cases

--- app/sidekiq/filter_searches_job.rb
+++ app/sidekiq/filter_searches_job.rb
@@ -2,13 +2,26 @@ class FilterSearchesJob
   include Sidekiq::Job
 
   def perform(search_id)
-    searched_at = Search.find(search_id).created_at
-    searches = Search.where(created_at: (searched_at - 5.seconds)..searched_at)
-    all_keywords = searches.map(&:keywords) 
+    search = Search.find(search_id)
+    return if search.nil?
+    
+    searched_at = search.created_at
+    searches = Search.where(created_at: (searched_at - 30.seconds)..searched_at)
+    keywords_with_searches = searches.group_by {|search| search.keywords.delete(" ")[...-1]}
 
-    searches.each do |search|
-      search.delete if all_keywords.any?(/^#{search.keywords}.+/)
+    keywords_with_searches.each do |keywords, searches|
+      if keywords_with_searches.keys.any?(/^#{keywords}.+/)
+        searches.each do |unnecessary_search|
+          unnecessary_search.delete
+        end
+      end
     end
 
+    keywords_with_searches.each do |keywords, searches|
+      searches[1..].each do |unnecessary_search|
+        unnecessary_search.delete
+      end
+    end
+    
   end
 end
