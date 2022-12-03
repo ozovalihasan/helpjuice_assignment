75d496fc1e1b4f68318e41ad3e16130f1681e8aa
ozovalihasan <ozovalihasan@gmail.com>
2022-12-03 18:56:06 +0300

    Add a test for FilterSearchesJob

--- spec/sidekiq/filter_searches_job_spec.rb
+++ spec/sidekiq/filter_searches_job_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
+
 RSpec.describe FilterSearchesJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "removes incomplete searches" do
+    Search.create(keywords: "mo")
+    Search.create(keywords: "mock")
+
+    expect(Search.count).to be 2
+    
+    described_class.new.perform(Search.find_by(keywords: "mock").id)
+
+    expect(Search.first.keywords).to eq "mock"
+    expect(Search.count).to be 1
+    
+  end
 end
